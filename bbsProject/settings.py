f057be573d98375563a81c7e603fef71a83b3b5c
kofuku yusuke <yusuke07@DESKTOP-B1D7MH6.localdomain>
2023-02-09 17:14:47 +0900

    2/9

--- bbsProject/settings.py
+++ bbsProject/settings.py
@@ -20,11 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+from .local_settings import SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -36,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bbsApp',
 ]
 
 MIDDLEWARE = [
